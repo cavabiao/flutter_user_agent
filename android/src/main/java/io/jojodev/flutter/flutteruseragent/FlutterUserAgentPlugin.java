0f213ab4e9c36f1d510d70de0fe47992cdb3d635
cavabiao <cavabiao@gmail.com>
2021-09-10 16:54:31 +0800

    add:Upgrade to Android V2

--- android/src/main/java/io/jojodev/flutter/flutteruseragent/FlutterUserAgentPlugin.java
+++ android/src/main/java/io/jojodev/flutter/flutteruseragent/FlutterUserAgentPlugin.java
@@ -11,106 +11,128 @@ import android.webkit.WebView;
 import java.util.HashMap;
 import java.util.Map;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterUserAgentPlugin */
-public class FlutterUserAgentPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_user_agent");
-    channel.setMethodCallHandler(new FlutterUserAgentPlugin(registrar.context()));
-  }
+/**
+ * FlutterUserAgentPlugin
+ */
+public class FlutterUserAgentPlugin implements MethodCallHandler, FlutterPlugin {
+    private static String CHANNEL_NAME = "flutter_user_agent";
+//
+//    /**
+//     * Plugin registration.
+//     */
+//    public static void registerWith(Registrar registrar) {
+//        final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_NAME);
+//        channel.setMethodCallHandler(new FlutterUserAgentPlugin(registrar.context()));
+//    }
 
-  private final Context context;
-  private Map<String, Object> constants;
+    private Context context;
+    private Map<String, Object> constants;
 
-  private FlutterUserAgentPlugin(Context context) {
-    this.context = context;
-  }
+    private MethodChannel channel;
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getProperties")) {
-      result.success(getProperties());
-    } else {
-      result.notImplemented();
-    }
-  }
+//    private FlutterUserAgentPlugin(Context context) {
+//        this.context = context;
+//    }
 
-  private Map<String, Object> getProperties() {
-    if(constants != null) {
-      return constants;
-    }
-    constants = new HashMap<>();
-
-    PackageManager packageManager = this.context.getPackageManager();
-    String packageName = this.context.getPackageName();
-    String shortPackageName = packageName.substring(packageName.lastIndexOf(".") + 1);
-    String applicationName = "";
-    String applicationVersion = "";
-    int buildNumber = 0;
-    String userAgent = this.getUserAgent();
-    String packageUserAgent = userAgent;
-
-    try {
-      PackageInfo info = packageManager.getPackageInfo(packageName, 0);
-      applicationName = this.context.getApplicationInfo().loadLabel(this.context.getPackageManager()).toString();
-      applicationVersion = info.versionName;
-      buildNumber = info.versionCode;
-      packageUserAgent = shortPackageName + '/' + applicationVersion + '.' + buildNumber + ' ' + userAgent;
-
-    } catch(PackageManager.NameNotFoundException e) {
-      e.printStackTrace();
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getProperties")) {
+            result.success(getProperties());
+        } else {
+            result.notImplemented();
+        }
     }
 
-    constants.put("systemName", "Android");
-    constants.put("systemVersion", Build.VERSION.RELEASE);
-    constants.put("packageName", packageName);
-    constants.put("shortPackageName", shortPackageName);
-    constants.put("applicationName", applicationName);
-    constants.put("applicationVersion", applicationVersion);
-    constants.put("applicationBuildNumber", buildNumber);
-    constants.put("packageUserAgent", packageUserAgent);
-    constants.put("userAgent", userAgent);
-    constants.put("webViewUserAgent", this.getWebViewUserAgent());
+    private Map<String, Object> getProperties() {
+        if (constants != null) {
+            return constants;
+        }
+        constants = new HashMap<>();
 
-    return constants;
-  }
+        PackageManager packageManager = this.context.getPackageManager();
+        String packageName = this.context.getPackageName();
+        String shortPackageName = packageName.substring(packageName.lastIndexOf(".") + 1);
+        String applicationName = "";
+        String applicationVersion = "";
+        int buildNumber = 0;
+        String userAgent = this.getUserAgent();
+        String packageUserAgent = userAgent;
 
-  private String getUserAgent() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      return System.getProperty("http.agent");
+        try {
+            PackageInfo info = packageManager.getPackageInfo(packageName, 0);
+            applicationName = this.context.getApplicationInfo().loadLabel(this.context.getPackageManager()).toString();
+            applicationVersion = info.versionName;
+            buildNumber = info.versionCode;
+            packageUserAgent = shortPackageName + '/' + applicationVersion + '.' + buildNumber + ' ' + userAgent;
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        constants.put("systemName", "Android");
+        constants.put("systemVersion", Build.VERSION.RELEASE);
+        constants.put("packageName", packageName);
+        constants.put("shortPackageName", shortPackageName);
+        constants.put("applicationName", applicationName);
+        constants.put("applicationVersion", applicationVersion);
+        constants.put("applicationBuildNumber", buildNumber);
+        constants.put("packageUserAgent", packageUserAgent);
+        constants.put("userAgent", userAgent);
+        constants.put("webViewUserAgent", this.getWebViewUserAgent());
+
+        return constants;
     }
 
-    return "";
-  }
+    private String getUserAgent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return System.getProperty("http.agent");
+        }
 
-  private String getWebViewUserAgent() {
-    if (Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1) {
-      return WebSettings.getDefaultUserAgent(this.context);
+        return "";
     }
 
-    WebView webView = new WebView(this.context);
-    String userAgentString = webView.getSettings().getUserAgentString();
+    private String getWebViewUserAgent() {
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1) {
+            return WebSettings.getDefaultUserAgent(this.context);
+        }
 
-    this.destroyWebView(webView);
+        WebView webView = new WebView(this.context);
+        String userAgentString = webView.getSettings().getUserAgentString();
 
-    return userAgentString;
-  }
+        this.destroyWebView(webView);
 
-  private void destroyWebView(WebView webView) {
-    webView.loadUrl("about:blank");
-    webView.stopLoading();
+        return userAgentString;
+    }
 
-    webView.clearHistory();
-    webView.removeAllViews();
-    webView.destroyDrawingCache();
+    private void destroyWebView(WebView webView) {
+        webView.loadUrl("about:blank");
+        webView.stopLoading();
 
-    // NOTE: This can occasionally cause a segfault below API 17 (4.2)
-    webView.destroy();
-  }
+        webView.clearHistory();
+        webView.removeAllViews();
+        webView.destroyDrawingCache();
+
+        // NOTE: This can occasionally cause a segfault below API 17 (4.2)
+        webView.destroy();
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL_NAME);
+        channel.setMethodCallHandler(this);
+        context = flutterPluginBinding.getApplicationContext();
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel.setMethodCallHandler(null);
+        context = null;
+    }
 }
